d11e34dea8cf91d617e860405d5e0b6f15560d5e
ViraVarguliak <varvera1486@gmail.com>
2025-04-12 16:30:20 +0200

    lesson19

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,25 @@
 import { useContext } from "react";
-import { HomeWrapper, Title } from "./styles";
 import { JokeContext } from "../../components/Layout/Layout";
+import { ErrorTitle, HomeWrapper, Title } from "./styles";
 import Button from "../../components/Button/Button";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../../components/Spinner/Spinner";
+
 
 function Home() {
-  const {error, isLoading, getJoke} = useContext(JokeContext)
-  
+const {error, isLoading, getJoke} = useContext(JokeContext)
+const navigate = useNavigate();
+const getJokeAndRedirect = ()=>{
+  getJoke()
+  navigate('/about')
+}
   return (
     <HomeWrapper>
-      <Title>Welcome to React Project</Title>
-      <Button name='GET JOKE' onClick={getJoke}/>
+       <Title>Welcome to React Project</Title>
+      <Button name='GET JOKE' onClick={getJokeAndRedirect}/>
+      {isLoading? <Spinner/> : null}
+      <ErrorTitle>Oops! Something broke{error}</ErrorTitle> 
     </HomeWrapper>
   )
 }
-
 export default Home;
